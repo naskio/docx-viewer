306367a1f4d32266d68e4e6d816eb94951ca4979
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2021-11-14 20:21:22 +0100

    BREAKING CHANGE: reloading the frame every few seconds (RELOAD_INTERVAL var) without listening to file changes

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ require('dotenv').config()
 const PORT = 3000;
 const GENERATED_FILENAME = process.env.NODE_ENV || "sample.docx";
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const RELOAD_INTERVAL = process.env.RELOAD_INTERVAL || 2500;
 
 assert(PUBLIC_URL, "PUBLIC_URL is not defined, please configure the .env file");
 
@@ -19,7 +20,7 @@ assert(PUBLIC_URL, "PUBLIC_URL is not defined, please configure the .env file");
 const liveReloadServer = livereload.createServer({
     exts: ['doc', 'docx']
 });
-liveReloadServer.watch(path.join(__dirname, 'build', GENERATED_FILENAME));
+liveReloadServer.watch(path.join(__dirname, 'dont_listen_to_build', GENERATED_FILENAME));
 
 const app = express()
 // injecting LiveReload client snippet
@@ -30,7 +31,8 @@ app.engine('html', ejs.renderFile);
 app.get('/', (req, res) => {
     res.render(path.join(__dirname, 'public', 'index.html'), {
         PUBLIC_URL: PUBLIC_URL,
-        GENERATED_FILENAME: GENERATED_FILENAME
+        GENERATED_FILENAME: GENERATED_FILENAME,
+        RELOAD_INTERVAL: RELOAD_INTERVAL
     });
 })
 
