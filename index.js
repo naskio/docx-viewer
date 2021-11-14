775f91ab8ff1273e388ca29351d3e847c5019234
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2021-11-14 20:07:39 +0100

    feat: add server with live reload
    - index page to display the generated docx file
    - listening to this docx file changes
    - reloading the index page on change event

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const path = require('path')
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
+const ejs = require('ejs');
+const assert = require("assert");
+
+// loading environment variables
+require('dotenv').config()
+
+// constants
+const PORT = 3000;
+const GENERATED_FILENAME = process.env.NODE_ENV || "sample.docx";
+const PUBLIC_URL = process.env.PUBLIC_URL;
+
+assert(PUBLIC_URL, "PUBLIC_URL is not defined, please configure the .env file");
+
+// live reload server, listening to docx file changes
+const liveReloadServer = livereload.createServer({
+    exts: ['doc', 'docx']
+});
+liveReloadServer.watch(path.join(__dirname, 'build', GENERATED_FILENAME));
+
+const app = express()
+// injecting LiveReload client snippet
+app.use(connectLivereload());
+
+// serving index.html to view the generated docx file
+app.engine('html', ejs.renderFile);
+app.get('/', (req, res) => {
+    res.render(path.join(__dirname, 'public', 'index.html'), {
+        PUBLIC_URL: PUBLIC_URL,
+        GENERATED_FILENAME: GENERATED_FILENAME
+    });
+})
+
+// serving docx files
+app.use('/static', express.static(path.join(__dirname, 'build')))
+
+// starting server
+app.listen(PORT, () => {
+    console.log(`listening at http://localhost:${PORT}`)
+})
